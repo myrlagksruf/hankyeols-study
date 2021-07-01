605b88267a0e779f6d8419817e715bb3c7e8bff7
myrlagksruf <myrlagksruf@gmail.com>
2021-07-01 12:31:03 +0900

    샘플 추가

--- tensorflow.js/chapter2/boston/src/data.ts
+++ tensorflow.js/chapter2/boston/src/data.ts
@@ -18,9 +18,12 @@ export class Boston{
     static model:tf.Sequential = null;
     static LEARNING_RATE = 0.001 as const;
     static BATCH_SIZE = 128 as const;
-    static NUM_EPOCHS = 400 as const;
+    static NUM_EPOCHS = 1000 as const;
+    static dataMean:tf.Tensor1D = null;
+    static dataStd:tf.Tensor1D = null;
     constructor(){}
-    static async load(){
+    static async ready(){
+        await tf.ready();
         const arr:Promise<[strBoston ,tf.Tensor2D]>[] = [];
 
         const loadFun = async (str:strBoston):Promise<[strBoston ,tf.Tensor2D]> => {
@@ -46,6 +49,8 @@ export class Boston{
 
         const { dataMean, dataStd } = this.determineMeanAndStddev(this.trainData);
 
+        this.dataMean = dataMean;
+        this.dataStd = dataStd;
         this.trainFeatures = this.normalizeTensor(this.trainData, dataMean, dataStd);
         this.testFeatures = this.normalizeTensor(this.testData, dataMean, dataStd);
 
@@ -80,6 +85,19 @@ export class Boston{
     }
     static normalizeTensor(data:tf.Tensor2D, dataMean:tf.Tensor1D, dataStd:tf.Tensor1D) {
         return data.sub<tf.Tensor2D>(dataMean).div<tf.Tensor2D>(dataStd);
+    }
+    static async test(obj:number[]){
+        const data = tf.tensor2d([obj])
+        const norm = this.normalizeTensor(data, this.dataMean, this.dataStd);
+        (this.model.predict(norm) as tf.Tensor2D).print();
+    }
+    static async load(url:string){
+        const model = (await tf.loadLayersModel(url)) as tf.Sequential;
+        model.compile({
+            optimizer: tf.train.adam(Boston.LEARNING_RATE),
+            loss: 'meanSquaredError'
+        });
+        Boston.model = model;
     }
     static async train(){
         const model = this.model;
--- tensorflow.js/chapter2/boston/src/index.ts
+++ tensorflow.js/chapter2/boston/src/index.ts
@@ -1,17 +1,42 @@
-import * as tf from '@tensorflow/tfjs';
 import { Boston } from './data';
 
 (async()=>{
-    await tf.ready();
-    await Boston.load();
-    button.classList.add('load');
+    await Boston.ready();
+    butCon.classList.add('load');
 })();
 
-const button = document.querySelector('button');
-button.addEventListener('click', async e => {
-    if(button.classList.contains('load') && !button.classList.contains('loading')){
-        button.classList.add('loading');
-        await Boston.train();
-        button.classList.remove('loading');
+const butCon = document.querySelector<HTMLDivElement>('#but-con');
+const load = document.querySelector<HTMLButtonElement>('#load');
+const save = document.querySelector<HTMLButtonElement>('#save');
+const train = document.querySelector<HTMLButtonElement>('#train');
+const input = document.querySelector('input');
+
+butCon.addEventListener('click', e => {
+    const tar = e.target as HTMLButtonElement|HTMLDivElement;
+    if(tar === butCon || !butCon.classList.contains('load') || tar.classList.contains('loading')){
+        e.stopPropagation();
+    } else {
+        tar.classList.add('loading');
     }
-});
+}, {capture:true});
+
+train.addEventListener('click', async e => {
+    await Boston.train();
+    train.classList.remove('loading');
+}, {capture:true});
+
+load.addEventListener('click', async e => {
+    await Boston.load('../sample/boston-red-sox.json');
+    load.classList.remove('loading');
+}, {capture:true});
+
+save.addEventListener('click', async e => {
+    await Boston.model.save('downloads://boston-red-sox');
+    save.classList.remove('loading');
+}, {capture:true});
+
+input.addEventListener('keydown', e => {
+    if(e.key === 'Enter' && input.validity.valid){
+        Boston.test(input.value.split(',').map(v => Number(v)))
+    }
+})
